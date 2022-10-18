27aac4ea089b10e438e6538c033b887e11d4860c
aroyehunsegun20201 <aroyehunsegun20201@gmail.com>
2022-10-18 20:34:44 +0100

    updated

--- instance/config.py
+++ instance/config.py
@@ -5,6 +5,6 @@ ADMIN_EMAIL="myemail#yahoo.com"
 FLASK_RUN_PORT=8080
 SECRET_KEY='ehehess@3647'
 
-SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://segun2@127.0.0.1:8889/test' 
+SQLALCHEMY_DATABASE_URI='postgres://xyyyabvvjzknpw:5b3a0103d0011a706c1d090fb1d89d3ab4d710a3a539b644fe7e25574a3805f4@ec2-34-194-40-194.compute-1.amazonaws.com:5432/d3tnak2k0f3092' 
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 CSRF_TOKEN='hfggrenknswp'
