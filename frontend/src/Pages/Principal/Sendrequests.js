71affe25e918cf973832dda907ce93eeebe1765e
Oliveth96 <117912797+Olivetttte@users.noreply.github.com>
2022-11-12 10:08:22 +0100

    I added filter to the Task Location searchbox

--- frontend/src/Pages/Principal/Sendrequests.js
+++ frontend/src/Pages/Principal/Sendrequests.js
@@ -5,7 +5,7 @@ import Delete from '../../Media/delete.png'
 import Edit from '../../Media/edit.png'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { Locations } from './location';
 
 
 function Tasks() {
@@ -150,114 +150,204 @@ function Tasks() {
         setTaskList(newTaskList)
     }
 
+
+    const [data, setData] = React.useState([]);
+    const [name, setName] = React.useState("");
+    
+    const filtered = Locations.filter((dt) =>
+      dt.name.toLowerCase().includes(name.toLowerCase())
+    );
+    React.useEffect(() => {
+      //
+    }, []);
+
+
     return (
-        
-        <div className="container">
-         <ToastContainer position="top-center" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
-            <div className="row" id='containers'>
-                <div className="col-md-6" id='taskfill'>
-                    <div className="card">
-                        <div className="card-header">
-                            <h3>Send Requests</h3>
-                        </div>
-                        <div className="card-body">
-                            <form onSubmit={handleFormSubmit}>
-                                <div className="form-group">
-                                    <label>Task Heading</label>
-                                    <input type="text" className="form-control" name="taskName" value={task.taskName} onChange={handleInputChange}  placeholder='Put in Subject'/>
-                                </div>
-                                <div className="form-group">
-                                    <label>Task Description</label>
-                                    <textarea placeholder='Enter task details' className="form-control" name="taskDescription" value={task.taskDescription} onChange={handleInputChange}  />
-                                </div>
-                                <div className="form-group">
-                                    <label>Task Start Date</label>
-                                    <input type="date" className="form-control" name="taskStartDate" value={task.taskStartDate} onChange={handleInputChange} />
-                                </div>
-                                <div className="form-group">
-                                    <label>Task End Date</label>
-                                    <input type="date" className="form-control" name="taskEndDate" value={task.taskEndDate} onChange={handleInputChange} />
-                                </div>
-                                {/* <div className="form-group">
+      <div className="container">
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <div className="row" id="containers">
+          <div className="col-md-6" id="taskfill">
+            <div className="card">
+              <div className="card-header">
+                <h3>Send Requests</h3>
+              </div>
+              <div className="card-body">
+                <form onSubmit={handleFormSubmit}>
+                  <div className="form-group">
+                    <label>Task Heading</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="taskName"
+                      value={task.taskName}
+                      onChange={handleInputChange}
+                      placeholder="Put in Subject"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Task Description</label>
+                    <textarea
+                      placeholder="Enter task details"
+                      className="form-control"
+                      name="taskDescription"
+                      value={task.taskDescription}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Task Start Date</label>
+                    <input
+                      type="date"
+                      className="form-control"
+                      name="taskStartDate"
+                      value={task.taskStartDate}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Task End Date</label>
+                    <input
+                      type="date"
+                      className="form-control"
+                      name="taskEndDate"
+                      value={task.taskEndDate}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  {/* <div className="form-group">
                                     <label>Task Status</label>
                                     <input type="text" className="form-control" name="taskStatus" value={task.taskStatus} onChange={handleInputChange} />
                                 </div> */}
-                                <div className="form-group">
-                                    <label>Task Payment</label>
-                                    <input type="text" className="form-control" name="taskPrice" value={task.taskPrice} onChange={handleInputChange} />
-                                </div>
-                                <div className="form-group">
-                                    <label>Task Location</label>
-                                    <input type="text" className="form-control" name="taskLocation" value={task.taskLocation} onChange={handleInputChange} />
-                                </div>
-                                <div className="form-group">
-                                    <label>Task Assigned By</label>
-                                    <input type="text" className="form-control" name="taskAssignedBy" value={task.taskAssignedBy} onChange={handleInputChange} />
-                                </div>
-                                <div className="form-group">
-                                    <label>Task Created On</label >  
-                                    <input type="text" className="form-control" name="taskCreatedOn" value={task.taskCreatedOn} onChange={handleInputChange} placeholder='The date is automatically selected onclicking submit'/>
-                                </div>
-                                {/* <div className="form-group">
+                  <div className="form-group">
+                    <label>Task Payment</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="taskPrice"
+                      value={task.taskPrice}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Task Location</label>
+                    <input
+                      type="text"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                    {name.length !== 0 && (
+                      <div>
+                        {filtered.slice(0, 3).map((dt) => {
+                          return <p key={dt.id}>{dt.name}</p>;
+                        })}
+                      </div>
+                    )}
+                  </div>
+                  <div className="form-group">
+                    <label>Task Assigned By</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="taskAssignedBy"
+                      value={task.taskAssignedBy}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Task Created On</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="taskCreatedOn"
+                      value={task.taskCreatedOn}
+                      onChange={handleInputChange}
+                      placeholder="The date is automatically selected onclicking submit"
+                    />
+                  </div>
+                  {/* <div className="form-group">
                                     <label>Task Updated On</label>
                                     <input type="text" className="form-control" name="taskUpdatedOn" value={task.taskUpdatedOn} onChange={handleInputChange} />
                                 </div> */}
-                                <button type="submit" className="btn btn-primary">Submit</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-              <div className="col-md-6">
-                    <div className="card" id='editrequests'>
-                        <div className="card-header">
-                            <h3>Requests</h3>
-                        </div>
-                        <div className="card-body">
-                            <table className="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>  Heading</th>
-                                        <th>  Description</th>
-                                        <th> Start Date</th>
-                                        <th>  End Date</th>
-                                        {/* <th>Task Status</th> */}
-                                        <th>  Payment</th>
-                                        <th>  Location</th>
-                                        <th>Task Assigned By</th>
-                                        <th>Date</th>
-                                        {/* <th>Task Updated On</th> */}
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        taskList.map((task, index) => (
-                                            <tr key={index}>
-                                                <td>{task.taskName}</td>
-                                                <td>{task.taskDescription}</td>
-                                                <td>{task.taskStartDate}</td>
-                                                <td>{task.taskEndDate}</td>
-                                                {/* <td>{task.taskStatus}</td> */}
-                                                <td>{task.taskPrice}</td>
-                                                <td>{task.taskLocation}</td>
-                                                <td>{task.taskAssignedBy}</td>
-                                                <td>{task.taskCreatedOn}</td>
-                                                {/* <td>{task.taskUpdatedOn}</td> */}
-                                                <td>
-                                                    <img src={Edit} alt="edit" onClick={() => handleEdit(index)} style={{width:'30%', marginLeft:'10px', marginRight:'10px'}} /> 
-                                                    <img src={Delete} alt="delete" onClick={() => handleDelete(index)} style={{width:'30%'}} />
-                                                </td>
-                                            </tr>
-                                        ))
-                                    }
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div> 
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </form>
+              </div>
             </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card" id="editrequests">
+              <div className="card-header">
+                <h3>Requests</h3>
+              </div>
+              <div className="card-body">
+                <table className="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th> Heading</th>
+                      <th> Description</th>
+                      <th> Start Date</th>
+                      <th> End Date</th>
+                      {/* <th>Task Status</th> */}
+                      <th> Payment</th>
+                      <th> Location</th>
+                      <th>Task Assigned By</th>
+                      <th>Date</th>
+                      {/* <th>Task Updated On</th> */}
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {taskList.map((task, index) => (
+                      <tr key={index}>
+                        <td>{task.taskName}</td>
+                        <td>{task.taskDescription}</td>
+                        <td>{task.taskStartDate}</td>
+                        <td>{task.taskEndDate}</td>
+                        {/* <td>{task.taskStatus}</td> */}
+                        <td>{task.taskPrice}</td>
+                        <td>{task.taskLocation}</td>
+                        <td>{task.taskAssignedBy}</td>
+                        <td>{task.taskCreatedOn}</td>
+                        {/* <td>{task.taskUpdatedOn}</td> */}
+                        <td>
+                          <img
+                            src={Edit}
+                            alt="edit"
+                            onClick={() => handleEdit(index)}
+                            style={{
+                              width: "30%",
+                              marginLeft: "10px",
+                              marginRight: "10px",
+                            }}
+                          />
+                          <img
+                            src={Delete}
+                            alt="delete"
+                            onClick={() => handleDelete(index)}
+                            style={{ width: "30%" }}
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Tasks
---
+++ frontend/src/Pages/Principal/location.js
@@ -0,0 +1,314 @@
+export const Locations = [
+    {
+        "id": 1,
+        "name": "Agege",
+    },
+    {
+        "id": 2,
+        "name": "Gbagada"
+    },
+    {
+        "id": 3,
+        "name": "Dopemu",
+    },
+    {
+        "id": 4,
+        "name": "Ikeja",
+    },
+    {
+        "id": 5,
+        "name": "Maryland",
+    },
+    {
+        "id": 6,
+        "name": "Obalende",
+    },
+    {
+        "id": 7,
+        "name": "Lagos Island",
+    },
+    {
+        "id": 8,
+        "name": "Victoria Island"
+    },
+    {
+        "id": 9,
+        "name": "Okota",
+    },
+    {
+        "id": 10,
+        "name": "Oshodi",
+    },
+    {
+        "id": 11,
+        "name": "Surulere",
+    },
+    {
+        "id": 12,
+        "name": "Masha",
+    },
+    {
+        "id": 11,
+        "name": "Kilo",
+    },
+    {
+        "id": 12,
+        "name": "Stadium",
+    },
+    {
+        "id": 13,
+        "name": "Ogba",
+    },
+    {
+        "id": 14,
+        "name": "kilo",
+    },
+    {
+        "id": 15,
+        "name": "Sabo",
+    },
+    {
+        "id": 16,
+        "name": "Ikotun",
+    },
+    {
+        "id": 17,
+        "name": "Ijegun",
+    },
+    {
+        "id": 18,
+        "name": "Lekki",
+    },
+    {
+        "id":19,
+        "name": "Ajah",
+    },
+    {
+        "id": 20,
+        "name": "Ozumba Mbadiwe",
+    },
+    {
+        "id": 21,
+        "name": "Alagbado",
+    },
+    {
+        "id": 22,
+        "name": "Ago Palace way",
+    },
+    {
+        "id": 23,
+        "name": "Festac Town",
+    },
+    {
+        "id": 24,
+        "name": "Mile 2",
+    },
+    {
+        "id": 25,
+        "name": "Apapa",
+    },
+    {
+        "id": 26,
+        "name": "Mushin",
+    },
+    {
+        "id": 27,
+        "name": "Ejigbo",
+    },
+    {
+        "id": 28,
+        "name": "Ikorodu",
+    },
+    {
+        "id": 29,
+        "name": "Eko Atlantic",
+    },
+    {
+        "id": 30,
+        "name": "Ikoyi",
+    },
+    {
+        "id": 31,
+        "name": "Ojo",
+    },
+    {
+        "id": 32,
+        "name": "Amuwo Odofin",
+    },
+    {
+        "id": 33,
+        "name": "Epe",
+    },
+    {
+        "id": 34,
+        "name": "Badagry",
+    },
+    {
+        "id": 35,
+        "name": "Shomolu",
+    },
+    {
+        "id": 36,
+        "name": "Abule egba",
+    },
+    {
+        "id": 37,
+        "name": "Abule Oja",
+    },
+    {
+        "id": 38,
+        "name": "Ikosi-Isheri",
+    },
+    {
+        "id": 39,
+        "name": "Agbado/Oke-Odo",
+    },
+    {
+        "id": 40,
+        "name": "Ayobo-Ipaja",
+    },
+    {
+        "id": 41,
+        "name": "Bariga",
+    },
+    {
+        "id": 42,
+        "name": "Egbe-Idimu",
+    },
+    {
+        "id": 43,
+        "name": "Ikosi-Isheri",
+    },
+    {
+        "id": 44,
+        "name": "Isolo",
+    },
+    {
+        "id": 45,
+        "name": "Onigbongbo",
+    },
+    {
+        "id": 46,
+        "name": "Mosan-Okunola",
+    },
+    {
+        "id": 47,
+        "name": "Odi Olowo-Ojuwoye",
+    },
+    {
+        "id": 48,
+        "name": "Ojodu",
+    },
+    {
+        "id": 49,
+        "name": "Onigbongbo",
+    },
+    {
+        "id": 50,
+        "name": "Ojokoro",
+    },
+    {
+        "id": 51,
+        "name": "Alagomeji",
+    },
+    {
+        "id": 52,
+        "name": "Iwaya",
+    },
+    {
+        "id": 53,
+        "name": "Oko-Baba",
+    },
+    {
+        "id": 54,
+        "name": "Alimosho",
+    },
+    {
+        "id": 55,
+        "name": "Ajeromi‑Ifelodun",
+    },
+    {
+        "id": 56,
+        "name": "Lagos Mainland",
+    },
+    {
+        "id": 57,
+        "name": "Eti-Osa",
+    },
+    {
+        "id": 58,
+        "name": "Ibeju-Lekki",
+    },
+    {
+        "id": 59,
+        "name": "Coker-Aguda",
+    },
+    {
+        "id": 60,
+        "name": "Iba",
+    },
+    {
+        "id": 61,
+        "name": "Ifelodun",
+    },
+    {
+        "id": 62,
+        "name": "Ijede",
+    },
+    {
+        "id": 63,
+        "name": "Ikosi/Isheri",
+    },
+    {
+        "id": 64,
+        "name": "Imota",
+    },
+    {
+        "id": 65,
+        "name": "Iru, Victoria Island",
+    },
+    {
+        "id": 66,
+        "name": "Ikosi Enjirin",
+    },
+    {
+        "id": 67,
+        "name": "Itire ikate",
+    },
+    {
+        "id": 68,
+        "name": "Lagos Island East",
+    },
+    {
+        "id": 69,
+        "name": "Oriade",
+    },
+    {
+        "id": 70,
+        "name": "Oto Awori",
+    },
+    {
+        "id": 71,
+        "name": "Olorunda",
+    },
+    {
+        "id": 72,
+        "name": "Ojuelegba",
+    },
+    {
+        "id": 73,
+        "name": "Ifako-Ijaiye",
+    },
+    {
+        "id": 74,
+        "name": "Kosofe",
+    },
+    {
+        "id": 75,
+        "name": "Somolu",
+    },
+    {
+        "id": 76,
+        "name": "Oyingbo",
+    }
+]
